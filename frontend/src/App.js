26c54db0808ca32805caa7a0e0fe5bd807adf561
mlw6 <mattwalowski@gmail.com>
2020-11-12 02:10:56 -0600

    Modify frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { MovieCardContainer } from './components/MovieCardContainer'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => 
+{
+	const cardData = [{id: 1, text:"Movie 1"}, {id:2, text:"Movie 2"}, {id:3, text:"Movie 3"}]
+	
+	return (
+		<div className="App">
+			<DndProvider backend={HTML5Backend}>
+				<MovieCardContainer cards={cardData} />
+			</DndProvider>
+		</div>
+	)
 }
 
 export default App;
