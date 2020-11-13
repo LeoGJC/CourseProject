7de071a75bea2c4aba622418e8fa1ecd60dc4caf
mlw6 <mattwalowski@gmail.com>
2020-11-12 19:35:56 -0600

    Change DND component

--- frontend/src/components/MovieCardContainer.js
+++ frontend/src/components/MovieCardContainer.js
@@ -1,34 +1,37 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Card } from './Card';
-import update from 'immutability-helper';
+import React, { useEffect } from "react";
+import RLDD from "react-list-drag-and-drop/lib/RLDD";
+import { useState } from 'react'
+import "./Card.css";
 
-const style = {
-    width: "30%",
-};
+const MovieCardContainer = (props) => {
+	const [movies, setMovies] = useState(props.movies);
 
-export const MovieCardContainer = (props) => 
-{
-	useEffect(() => {
-		setCards(props.cards);
-		console.log(props.cards)
-	}, [props]);
+    useEffect(() => {
+		setMovies(props.movies)
+	}, [props.movies])
 
-	const [cards, setCards] = useState(props.cards);
-	const moveCard = useCallback((dragIndex, hoverIndex) => {
-		const dragCard = props.cards[dragIndex];
-		setCards(update(props.cards, {
-			$splice: [
-			[dragIndex, 1],
-			[hoverIndex, 0, dragCard],
-		],
-	    }));
-	}, [props.cards]);
-	    
-	const renderCard = (card, index) => {
-	    return (<Card key={card.id} index={index} id={card.id} text={card.text} moveCard={moveCard}/>);
-	};
+  const itemRenderer = (item, index) => {
+    return (
+      <div className="item">
+        <p className="title">{item.title}</p>
+        <p className="body">{item.body}</p>
+        <div className="small">
+          item.id: {item.id} - index: {index}
+        </div>
+      </div>
+    );
+  }
 
-	return (<>
-		    <div style={style}>{cards.map((card, i) => renderCard(card, i))}</div>
-		</>);
-};
+    return (
+      <div>
+        <RLDD
+          cssClasses="example"
+          items={movies}
+          itemRenderer={itemRenderer}
+          onChange={setMovies}
+        />
+      </div>
+    );
+}
+
+export default MovieCardContainer;
--- frontend/src/components/MovieSelector.js
+++ frontend/src/components/MovieSelector.js
@@ -1,16 +1,17 @@
-import { MovieCardContainer } from './MovieCardContainer'
+import MovieCardContainer from './MovieCardContainer'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useState } from 'react'
 
 const MovieSelector = () => {
-    const [cardData, setCardData] = useState([{id: 1, text:"Movie 1"}, {id:2, text:"Movie 2"}, {id:3, text:"Movie 3"}]);
+    const [cardData, setCardData] = useState([{id: 1, title:"Movie 1", index: 1}, {id:2, title:"Movie 2", index: 2}, {id:3, title:"Movie 3", index:1}]);
     const [movie, setMovie] = useState("");
 
     const OnAddMovie = () => {
         var movieData = {
             id: cardData.length + 1,
-            text: movie,
+            title: movie,
+            index: cardData.length + 1,
         }
 
         setCardData(cardData.concat(movieData));
@@ -23,7 +24,7 @@ const MovieSelector = () => {
                     <input type="text" name="add-movie" onChange={event => setMovie(event.target.value)}/>
                     <input type="submit" text="Add Movie" onClick={OnAddMovie} />
                 </div>
-                <MovieCardContainer cards={cardData} />
+                <MovieCardContainer movies={cardData} />
             </DndProvider>
         </div>
     );
