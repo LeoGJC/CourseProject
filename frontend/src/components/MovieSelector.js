b67e787badd3c7a3cb2d4072057754e60072ac08
mlw6 <mattwalowski@gmail.com>
2020-11-16 17:56:58 -0600

    Send request to backend

--- frontend/src/components/MovieSelector.js
+++ frontend/src/components/MovieSelector.js
@@ -1,4 +1,5 @@
 import MovieCardContainer from './MovieCardContainer'
+import { SUBMIT_ENDPOINT } from './../constants'
 import { useState } from 'react'
 import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import './MovieSelector.css';
@@ -23,6 +24,15 @@ const MovieSelector = () => {
         setMovie("");
     }
 
+    const SubmitMovies = () => {
+        fetch(SUBMIT_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify(cardData),
+        }).then(res => {
+            // TODO: Display results
+        })
+    }
+
     return (
         <div id='movie-selector-container' className='movie-selector-container-w' >
                 <InputGroup>
@@ -37,7 +47,7 @@ const MovieSelector = () => {
                     </InputGroup.Append>
                 </InputGroup>
                 <MovieCardContainer movies={cardData} />
-                <Button disabled={cardData.length < 2} id='result-button'>See Results</Button>
+                <Button disabled={cardData.length < 2} onClick={SubmitMovies} id='result-button'>See Results</Button>
         </div>
     );
 }
