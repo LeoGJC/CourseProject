1c0ce195e656412375cab230fe390cd286884ef1
mlw6 <mattwalowski@gmail.com>
2020-11-16 18:28:58 -0600

    Change backend port addr

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "http://localhost:5000";
 export const SUBMIT_ENDPOINT = `${BACKEND_URL}/submit`
 
 export const TEXT_LOADING = "Loading results";
