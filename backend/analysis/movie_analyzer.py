137e88a88dd4694ace89489b8702ddb090562e55
mlw6 <mattwalowski@gmail.com>
2020-11-16 19:27:06 -0600

    Pass movie list to stub

---
+++ backend/analysis/movie_analyzer.py
@@ -0,0 +1,7 @@
+# Movies: The list of movies and their rankings by their user. The 'index' field of a movie is its ranking
+# Example: [{'id': 1, 'title': 'Movie 1', 'index': 1}, {'id': 2, 'title': 'Movie 2', 'index': 2}, {'id': 3, 'title': 'Movie 3', 'index': 3}]
+def analyze_movies(movies):
+    # TODO: Analyze the movies and return a response
+    
+    # We need to decide what to return. Depends on how we want to present the results
+    return {'message': "Test Response"}
--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import jsonify, make_response
 from flask_cors import CORS
+from analysis.movie_analyzer import analyze_movies
 import json
 
 app = Flask(__name__)
@@ -9,8 +10,6 @@ CORS(app)
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.json
-    # TODO: Process data
-
-    resp_data = {'message': "Test Response"}
+    resp_data = analyze_movies(data)
     resp = make_response(jsonify(resp_data), 201)
     return resp
